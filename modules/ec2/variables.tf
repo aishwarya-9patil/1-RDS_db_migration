7d9826c00450ceec508b028c5d7106af34ccfc56
aishwarya-9patil <aishwarayagouda@gmail.com>
2025-06-21 22:53:56 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,6 +1,6 @@
 variable "ami_id" {
   type = string
-  default = "ami-05b10e08d247fb927"
+  default = "ami-09e6f87a47903347c"
 }
 
 variable "instance_type" {
@@ -10,10 +10,10 @@ variable "instance_type" {
 
 variable "subnet_id" {
   type = string 
-  default = "subnet-023f76c7148cae5f9"
+  default = "subnet-09caaf11453fcf182"
 }
 
 variable "vpc_id" {
   type = string 
-  default = "vpc-051c79ef4f13b9d1a"
+  default = "vpc-078f0207aafc7071b"
 }
