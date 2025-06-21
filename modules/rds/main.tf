6b39c7e3b05db617c25f3c595d59d6aeaf3e29cd
aishwarya-9patil <aishwarayagouda@gmail.com>
2025-06-21 23:20:44 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -48,4 +48,4 @@ resource "aws_db_instance" "rds" {
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name = aws_db_subnet_group.subnet_group.name
-}
+} 
